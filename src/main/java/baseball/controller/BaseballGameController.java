1bd574b464769c4722ab96ec46150382eb783ce4
leenayeonnn <skdus4190@gmail.com>
2024-10-11 20:46:59 +0900

    feat: 정답을 맞출 때 까지 반복되도록 수정

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -9,12 +9,20 @@ import domain.GameResult;
 public class BaseballGameController {
     public void run() {
         StartView.startComment();
-        Balls computerBalls = new Balls();
 
-        String inputNumbers = InputView.inputNumbers();
-        Balls userBalls = new Balls(inputNumbers);
+        while (true) {
+            Balls computerBalls = new Balls();
 
-        GameResult gameResult = new GameResult(computerBalls, userBalls);
-        OutputView.gameResult(gameResult);
+            String inputNumbers = InputView.inputNumbers();
+            Balls userBalls = new Balls(inputNumbers);
+
+            GameResult gameResult = new GameResult(computerBalls, userBalls);
+            OutputView.gameResult(gameResult);
+
+            if (gameResult.isAnswer()) {
+                OutputView.gameEnd();
+                break;
+            }
+        }
     }
 }
