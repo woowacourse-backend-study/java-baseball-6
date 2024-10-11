6ee3cb32b670df703187a37b6771628b418d17d8
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-10-11 21:46:52 +0900

    feat: 게임 진행 상태 저장 및 확인 기능 구현
    야구 게임 진행상태를 저장, 업데이트하고 이를 확인하는 필드와 메서드 추가하였습니다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,6 +5,9 @@ import static baseball.Output.printMessage;
 
 public class BaseballGame {
 
+    private boolean gameStatus = false;
+    // 게임이 진행중인지 상태를 저장했다가 게임이 종료되면 상태가 변경된다
+    // 이 상태를 게임 매니저가 보고있다가 게임이 종료되었다면 다시 실행할지 묻는다.
 
     public void start() {
         // 시작 안내 문구 출력
@@ -14,6 +17,7 @@ public class BaseballGame {
         NumbersGenerator numbersGenerator = new NumbersGenerator();
         Numbers gameNumbers = numbersGenerator.randomNumberGenerate();
         System.out.println(gameNumbers);
+
         do {
             referee.init();
             // 게임 번호 생성
@@ -27,16 +31,18 @@ public class BaseballGame {
             System.out.println(referee.getCountResult());
             // 심판이 정답 판단
             // 결과 출력
+            gameStatus = referee.getGameResult();
         }
-        while (!referee.getGameResult());
+        while (gameStatus);
 
         System.out.println("게임 완료");
+
         // 게임 종료 여부 판단
         // 게임 종료 시 -> 재시작, 종료 여부 입력 문구 출력
         // 게임 지속 시 -> 정답 번호 입력부터 다시 시작
     }
 
-    public void end() {
-
+    public boolean isEnd() {
+        return gameStatus;
     }
 }
