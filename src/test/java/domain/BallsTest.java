ed85ffee055ad3a75bfc05f6c7ba45f0e033b187
leenayeonnn <skdus4190@gmail.com>
2024-10-12 22:58:44 +0900

    style: 람다식으로 변경

--- src/test/java/domain/BallsTest.java
+++ src/test/java/domain/BallsTest.java
@@ -11,9 +11,7 @@ class BallsTest {
     @DisplayName("유저가 입력한 숫자 개수가 3 초과면 에러를 던짐")
     void inputBallOverLengthErrorTest() {
         String userInput = "1234";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Balls(userInput);
-        });
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Balls(userInput));
 
         assertEquals(Message.ERROR_INPUT_BALL_NUMBERS_LENGTH, exception.getMessage());
     }
@@ -22,9 +20,7 @@ class BallsTest {
     @DisplayName("유저가 입력한 숫자 개수가 3 미만이면 에러를 던짐")
     void inputBallUnderLengthErrorTest() {
         String userInput = "12";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Balls(userInput);
-        });
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Balls(userInput));
 
         assertEquals(Message.ERROR_INPUT_BALL_NUMBERS_LENGTH, exception.getMessage());
     }
@@ -33,9 +29,7 @@ class BallsTest {
     @DisplayName("유저가 입력한 숫자 범위가 1 ~ 9 가 아니면 에러를 던짐")
     void inputBallNumberRangeErrorTest() {
         String userInput = "012";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Balls(userInput);
-        });
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Balls(userInput));
 
         assertEquals(Message.ERROR_INPUT_BALL_NUMBERS_RANGE, exception.getMessage());
     }
@@ -44,9 +38,7 @@ class BallsTest {
     @DisplayName("유저가 입력한 숫자에 중복이 있으면 에러를 던짐")
     void inputBallNumberDuplicateErrorTest() {
         String userInput = "112";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Balls(userInput);
-        });
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Balls(userInput));
 
         assertEquals(Message.ERROR_INPUT_BALL_DUPLICATE, exception.getMessage());
     }
@@ -55,9 +47,7 @@ class BallsTest {
     @DisplayName("유저가 입력에 숫자가 아닌 문자가 있으면 에러를 던짐")
     void inputBallNotNumberErrorTest() {
         String userInput = "12a";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Balls(userInput);
-        });
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Balls(userInput));
 
         assertEquals(Message.ERROR_INPUT_BALL_ONLY_NUMBERS, exception.getMessage());
     }
