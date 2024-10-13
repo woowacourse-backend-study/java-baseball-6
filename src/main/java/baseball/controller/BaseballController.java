ab1b8ea8ed26f751a30ec45009fac15e3b5b67a5
yunjeooong <yunjeongssu@gmail.com>
2024-10-14 01:15:49 +0900

    fix:inputview 메서드이름변경

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,11 +1,17 @@
 package baseball.controller;
 
+import baseball.service.ScoreCalculator;
+import baseball.util.RandomNumbersMaker;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.List;
+
 public class BaseballController {
-    final OutputView outputView;
-    final InputView inputView;
+    private final OutputView outputView;
+    private final InputView inputView;
+
+    private ScoreCalculator scoreCalculator;
 
     public BaseballController(InputView inputView, OutputView outputView) {
         this.outputView = outputView;
@@ -14,5 +20,10 @@ public class BaseballController {
     }
     public void app(){
         outputView.printStartMessage();
+        List<Integer> computer = RandomNumbersMaker.maker();
+        System.out.println("computer = " + computer);
+        String getNumber = String.valueOf(inputView.getGuessNumber());
+
+        //List<Integer> computer
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private Scanner scanner = new Scanner(System.in);
-    public int printGetNumber() {
+    public int getGuessNumber() {
         System.out.println("숫자를 입력해 주세요: ");
         return scanner.nextInt();
     }
