ac89a91ce5b3b0ba620ca2d9a12577f08be18a81
yunjeooong <yunjeongssu@gmail.com>
2024-10-14 02:51:43 +0900

    fix:NumberValidator 관련 오류 수정

--- src/main/java/validator/NumberValidator.java
+++ src/main/java/validator/NumberValidator.java
@@ -1,14 +1,9 @@
 package validator;
 
-import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Validator;
-
 import java.util.HashSet;
 import java.util.Set;
 
-import static com.oracle.graal.compiler.enterprise.c.v;
-
-public class NumberValidator implements Validator{
-
+public class NumberValidator implements Validator {
 
     @Override
     public void validate(String value) {
@@ -17,17 +12,17 @@ public class NumberValidator implements Validator{
         validRange(value);
     }
 
-    //타입이 잘못 입력된 경우
-    private void validType(String value) throws IllegalAccessException {
-        try{
+    // 숫자 타입 검증
+    private void validType(String value) {
+        try {
             Integer.parseInt(value);
-        }catch (NumberFormatException e){
-            throw new IllegalAccessException("잘못된 숫자 입력입니다.")
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 숫자 입력입니다.");
         }
-
     }
-    //3이라는 길이가 벗어난 입력
-    private void validLength(String value){
+
+    // 길이 검증 (3자리 숫자 여부)
+    private void validLength(String value) {
         String[] splitValues = value.split("");
         Set<String> temp = new HashSet<>();
         for (String v : splitValues) {
@@ -38,8 +33,9 @@ public class NumberValidator implements Validator{
         }
     }
 
-    //1~9라는 범위에 벗어난 입력
-    private void validRange(String value){String[] splitValues = value.split("");
+    // 숫자 범위 검증 (1~9 사이의 값)
+    private void validRange(String value) {
+        String[] splitValues = value.split("");
         for (String v : splitValues) {
             int number = Integer.parseInt(v);
             if (number < 1 || number > 9) {
